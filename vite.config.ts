fcdee99a81d0bbd97c0370f60f7a84140c8990b0
Arie <rest7261@gmail.com>
2026-02-23 01:59:52 +0200

    Yoga site - UI + Developer badge

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    server: { open: true }
+  plugins: [react()],
+  base: "/yoga/",
+  server: { open: true }
 })
+
+
